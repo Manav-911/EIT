b2cc5e3174f1e677c88cefd9fe79fcc4bbe0384d
Manav-911 <parmarmanav409@gmail.com>
2025-08-11 22:51:04 +0530

    Added 2d overlay of cloth and cloth manual control features

--- bigShopping/src/App.jsx
+++ bigShopping/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import Webcam from 'react-webcam';
 import './css/App.css'
 import  {TryOn}  from './components/try-on.jsx';
-import  {Camera} from './components/Camera.jsx';
+import  {Camera} from './components/camera.jsx';
 
 export default function MyApp() {
   const [showCamera, setShowCamera] = useState(false);
--- bigShopping/src/components/camera.jsx
+++ bigShopping/src/components/camera.jsx
@@ -1,9 +1,14 @@
 import Webcam from "react-webcam";
 import React, { useRef, useState, useCallback } from "react";
+import clothImg from "../assets/cloth/tshirt-black.png";
+import '../css/Camera.css';
 
 function Camera() {
     const webcamRef = useRef(null);
     const [imgSrc, setImgSrc] = useState(null);
+
+    const [shirtTop, setShirtTop] = useState(10);
+    const [shirtLeft, setShirtLeft] = useState(25);
 
     const capture = useCallback(() => {
         const imageSrc = webcamRef.current.getScreenshot();
@@ -17,19 +22,54 @@ function Camera() {
     return (
         <div className="Camera">
             <h2>Camera</h2>
-            <Webcam
-                audio={false}
-                ref={webcamRef}
-                screenshotFormat="image.jepg"
-                width = {400}
-                height = {300}
-                videoConstraints = {
-                    {
-                        facingMode: "user"
+            <div className="webcam-container">
+                <Webcam
+                    audio={false}
+                    ref={webcamRef}
+                    screenshotFormat="image/jpeg"
+                    videoConstraints = {
+                        {
+                            facingMode: "user"
+                        }
                     }
-                }
-                />
-
+                    mirrored={true}
+                    className="webcam-feed"
+                    />
+                    <img
+                        src={clothImg}
+                        alt="Cloth_Overlay"
+                        className="cloth-overlay"
+                        style={
+                            {
+                                top: `${shirtTop}px`,
+                                left: `${shirtLeft}px`,
+                            }
+                        }
+                    />
+            </div>
+            <div className="overlay-controls">
+                <label>
+                    Top:
+                    <input
+                        type="range"
+                        min="50"
+                        max="850"
+                        value={shirtTop}
+                        onChange={e => setShirtTop(Number(e.target.value))}
+                    />
+                </label>
+                
+                <label>
+                    Left:
+                    <input
+                        type="range"
+                        min="50"
+                        max="850"
+                        value={shirtLeft}
+                        onChange={e => setShirtLeft(Number(e.target.value))}
+                    />
+                </label>
+            </div>
             <button onClick={capture}>Capture ScreenShot</button>
             {imgSrc && (
                 <div className="screenshot">
